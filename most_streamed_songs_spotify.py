b307868877110b64d5007b5bc02381d09cb2585e
prnvvv <prannavakhanth12@gmail.com>
2024-06-25 21:30:43 +0530

    Update most_streamed_songs_spotify.py

--- most_streamed_songs_spotify.py
+++ most_streamed_songs_spotify.py
@@ -1,8 +1,14 @@
-import numpy as np
 import pandas as pd
 
 filePath = r"C:\Users\Prannavakhanth\Documents\Machine-Learning\Most Streamed Spotify Songs 2024.csv"
 
-dataFrame = pd.read_csv(filePath)
+# Try different encodings if one fails
+try:
+    dataFrame = pd.read_csv(filePath, encoding='utf-8')
+except UnicodeDecodeError:
+    try:
+        dataFrame = pd.read_csv(filePath, encoding='latin1')
+    except UnicodeDecodeError:
+        dataFrame = pd.read_csv(filePath, encoding='cp1252')
 
-print(dataFrame.head(10))
+print(dataFrame.head())

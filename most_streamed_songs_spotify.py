fdd27224dcd927fb960e7f741054fccc6dfd2648
prnvvv <prannavakhanth12@gmail.com>
2024-06-25 21:30:06 +0530

    Update most_streamed_songs_spotify.py

--- most_streamed_songs_spotify.py
+++ most_streamed_songs_spotify.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-filePath = "C:\Users\Prannavakhanth\Documents\Machine-Learning\Most Streamed Spotify Songs 2024.csv"
+filePath = r"C:\Users\Prannavakhanth\Documents\Machine-Learning\Most Streamed Spotify Songs 2024.csv"
 
 dataFrame = pd.read_csv(filePath)
 
